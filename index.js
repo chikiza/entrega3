a0f4e3535152685e9f62a87a9cf76ee9aba4d7d6
chikiza <83772028+chikiza@users.noreply.github.com>
2024-05-28 19:03:11 -0400

    fetch

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const URL_BASE = "https://pokeapi.co/api/v2/";
 
-https: fetch(`${URL_BASE}pokemon/pikachu`)
+/*https: fetch(`${URL_BASE}pokemon/pikachu`)
 .then((response) => response.json())
 .then((data) => console.log(data))
 .catch(error => console.log('ERROR', error))
-.finally(console.log('termine'));
+.finally(console.log('termine'));*/
+
+const getPokemon = async() => {
+    try {
+        const response = await fetch(`${URL_BASE}pokemon/charizard`)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+
+    } finally {
+
+    }
+}
+getPokemon()
