0e07ea5676d28f467aad7316f3855374d3cbf44a
chikiza <83772028+chikiza@users.noreply.github.com>
2024-05-31 17:29:10 -0400

    funciona
    html y js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const API_URL = "https://jsonplaceholder.typicode.com/";
 
 const HTMLResponse = document.querySelector("#app");
 
+
 fetch(`${API_URL}/users`)
 .then((response)=> response.json())
 .then((users) =>{
-    const tpl = users.map(user => <li></li>)
+    const tpl = users.map(user => `<li>${user.name} === ${user.email}</li>`);
+    HTMLResponse.innerHTML =  `<ul>${tpl}</ul>`;
 })
